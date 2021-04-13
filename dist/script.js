031b7c7b428850b89ace8122c5e630a9c0fe7ef8
Magda Sz <pesahson@gmail.com>
2021-04-13 19:53:22 +0200

    test weather

--- dist/script.js
+++ dist/script.js
@@ -5,15 +5,10 @@ const div = document.querySelector('div')
 const getData = async() =>  {
 const response = await fetch(url);
 const data = await response.json();
-renderUser(data)
+console.log(data)
 }
 
 
- const renderUser = (users) => {
-  const usersString = users.map(element => {
-    return `<p>${element.name}</p>`
-  }).join("");
-  div.innerHTML = usersString;
- }
+ 
 
 getData()
--- functions/users.js
+++ functions/users.js
@@ -1,13 +1,32 @@
 const fetch = require("node-fetch");
 
+// exports.handler = async(event, context) => {
+//   const url = "https://jsonplaceholder.typicode.com/users";
+//   try {
+//     const users = await fetch(url);
+//     const usersJson = await users.json();
+//     return {
+//       statusCode: 200, 
+//       body: JSON.stringify(usersJson)
+//     };
+//   } catch (err) {
+//     return {
+//       statusCode: 422, 
+//       body: err.stack
+//     };
+//   }
+// }
+
+const { KEY_WEATHER }  = process.env;
+
 exports.handler = async(event, context) => {
-  const url = "https://jsonplaceholder.typicode.com/users";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=lisbon&appid=${KEY_WEATHER}&units=metric`;
   try {
-    const users = await fetch(url);
-    const usersJson = await users.json();
+    const data = await fetch(url);
+    const dataJson =  await data.json();
     return {
       statusCode: 200, 
-      body: JSON.stringify(usersJson)
+      body: JSON.stringify(dataJson)
     };
   } catch (err) {
     return {
@@ -15,4 +34,6 @@ exports.handler = async(event, context) => {
       body: err.stack
     };
   }
-}
+} 
+
+
