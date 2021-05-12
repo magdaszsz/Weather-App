8a80693565db6b0d4ea7be441376d9208524448d
Magda Sz <pesahson@gmail.com>
2021-05-12 18:17:05 +0200

    better ui

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,10 @@
 const url = "/.netlify/functions/users";
 const input = document.querySelector('input');
-const div = document.querySelector('h3');
+const temp = document.querySelector('.temp');
+const city = document.querySelector('.city')
+const desc = document.querySelector('.description')
 const btn = document.querySelector('button');
+const localTime = document.querySelector('.target__time')
 let userInput = '';
 let cityObj = {}
 
@@ -21,14 +24,30 @@ const response = await fetch(url, {
   body: JSON.stringify(cityObj)
 });
 const data = await response.json();
+showData(data)
 console.log(data)
-showData(data);
+ 
 }
 
 
  
 
 function showData(data) {
-  div.innerHTML = data.main.temp;
+  temp.innerHTML = `${Math.round(data.main.temp)} &deg;C`;
+  desc.innerText = data.weather[0].description;
+  city.innerHTML = `${data.name} <span class="country">${data.sys.country}</span>`;
+  input.value = '';
+
+  // const da = new Date().toISOString().split('T')[1].slice(0,5);
+  const off = data.timezone;
+  const offset = off / 60 / 60;
+ 
+ const targetTime = new Date(new Date().getTime() + offset * 3600 * 1000)
+   .toUTCString()
+   .replace(/ GMT$/, "");
+   const targetT = targetTime.slice(17);
+   console.log(targetT)
+  localTime.innerHTML = `<small>${targetTime.slice(0, 16)}</small>`
+ 
 }
 
